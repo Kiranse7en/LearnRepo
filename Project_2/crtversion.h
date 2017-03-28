abbd4bd12ff23e929544538f8d5459107b0d2533
kiran <kiru1317@gmail.com>
2017-03-28 23:41:59 +0530

    crtversion.h

--- Project_2/crtversion.h
+++ Project_2/crtversion.h
@@ -4,7 +4,7 @@
 *       Copyright (c) Microsoft Corporation. All rights reserved.
 *
 *Purpose:
-*       Contains macro definitions for the CRT version.
+*       Contains macro definitions for the 
 *       [System V]
 *
 *       [Public]
