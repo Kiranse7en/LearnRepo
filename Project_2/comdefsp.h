06d1c1b3d29539ca2469b565f07a5a8a33d36986
kiran <kiru1317@gmail.com>
2017-03-28 23:36:13 +0530

    comdefsp.h

--- Project_2/comdefsp.h
+++ Project_2/comdefsp.h
@@ -1,7 +1,7 @@
 /***
 * comdefsp.h - Native C++ compiler COM support - smart pointer support header
 *
-* Copyright (c) Microsoft Corporation. All rights reserved.
+* Copyright (c) Microsoft Corporation. 
 *
 ****/
 #define KILL 0
